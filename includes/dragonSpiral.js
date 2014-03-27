d85468ec7a2d984eb92d6827aa3e302d77c7351c
Yves Courtois <Courtois1337@gmail.com>
2014-03-26 20:30:53 -0500

    redraws on resize, cleaned up code

--- includes/dragonSpiral.js
+++ includes/dragonSpiral.js
@@ -2,56 +2,59 @@ $(document).ready(function() {
 
     // initializing necessary canvas variables
     var c = document.getElementById('c');
-    c.height = ~~(window.innerHeight * .99);
-    c.width = ~~(window.innerWidth * .99);
-
     var ctx = c.getContext("2d");
 
+    var direction;
+    var direction_x;
+    var direction_y;
+    var current_x;
+    var current_y;
+    var l;
 
+    function init() {
 
-    ctx.clearRect(0, 0, c.width, c.height);
+        c.height = ~~(window.innerHeight * .99);
+        c.width = ~~(window.innerWidth * .99);
+        ctx.clearRect(0, 0, c.width, c.height);
+        ctx.lineWidth=3;
 
-    ctx.beginPath();
-    ctx.strokeStyle = "#9CFF00";
+        // determines which way you go
+        direction = "east";
+        direction_x = 1;
+        direction_y = 0;
 
-    // determines which way you go
-    var direction = "east";
-    var direction_x = 1;
-    var direction_y = 0;
+        // current location on canvs
+        current_x = c.width /3 * 2;
+        current_y = c.height /3;
 
-    // current location on canvs
-    var current_x = c.width /3 * 2;
-    var current_y = c.height /3;
+        // len of each line in pixels
+        l = 8;
 
-    // len of each line in pixels
-    var l = 8;
+        dragonSpiral(12);
+    }
 
     function x(n) {
         if (n == 0)
             return;
+
         x(n-1);
         turnRight();
         y(n-1);
-        moveForward();    
+        moveForward();   
     }
 
     function y(n) {
         if (n == 0)
             return;
+
         moveForward();
         x(n-1);
-        // turn left here
         turnLeft();
         y(n-1);
     }
 
     function dragonSpiral(n) {
         moveForward();
-        // turnRight();
-        // moveForward();
-        // turnLeft()
-        // moveForward();
-
         x(n);
     }
 
@@ -60,7 +63,6 @@ $(document).ready(function() {
         ctx.moveTo(current_x, current_y);
        
         ctx.strokeStyle = randColor();
-        ctx.lineWidth=3;
         ctx.lineTo(direction_x * l + current_x, direction_y * l + current_y);
         ctx.stroke();
 
@@ -129,6 +131,10 @@ $(document).ready(function() {
         return color;   
     }
 
-    dragonSpiral(12);
+    init();
+
+    $(window).resize(function(){
+        init();
+    });
 });
 
